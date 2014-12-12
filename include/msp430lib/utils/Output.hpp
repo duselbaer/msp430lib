7329efab3e9d5b083e0a8de9bfcc660f53db77c5
Ronny Spiegel <ronny.spiegel@googlemail.com>
2014-12-12 18:32:24 +0100

    Output function for hex byte added

--- include/msp430lib/utils/Output.hpp
+++ include/msp430lib/utils/Output.hpp
@@ -21,12 +21,42 @@ class Output
         m_device.putc((uint8_t)*str++);
       }
     }
+
+    void putc(char c)
+    {
+      m_device.putc((uint8_t) c);
+    }
 };
 
 template<typename D>
 Output<D>& operator<<(Output<D>& os, const char* str)
 {
   os.puts(str);
+  return os;
+}
+
+template<typename D>
+void writeNibble(Output<D>& os, uint8_t nibble)
+{
+  nibble = nibble & 0x0F;
+  if (nibble >= 0x0A)
+  {
+    nibble = nibble - 10;
+    nibble = nibble + 'a';
+  }
+  else
+  {
+    nibble = nibble + '0';
+  }
+  os.putc((char) nibble);
+}
+
+template<typename D>
+Output<D>& operator<<(Output<D>& os, uint8_t const byte)
+{
+  os.puts("0x");
+  writeNibble(os, byte >> 4);
+  writeNibble(os, byte);
   return os;
 }
 }
